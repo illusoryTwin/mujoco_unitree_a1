0e91050bdf4f6b98ac7fd10e87a0830fe4991e32
Ekaterina <mozhegova04@gmail.com>
2024-05-16 17:56:30 +0300

    Some tests with control

--- src/a1_apply_control.py
+++ src/a1_apply_control.py
@@ -42,7 +42,11 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
             data.qpos[0] = 0.
             data.qpos[1] = 0.
             data.qpos[2] = 0.3
+            # data.qpos[3:7] = quaternion[0]
+            data.qpos[7:] = joint_angles[0]
    
-            data.ctrl = [100, -100, 100, 0, 0, 100, -100, 100, 0, 0, 10, 10]
-            
+            # data.ctrl = [100, -100, 100, 0, 0, 100, -100, 100, 0, 0, 10, 10]
+            data.ctrl = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            # data.ctrl = [100]* 100 #, -100, 100, 0, 0, 100, -100, 100, 0, 0, 10, 10]
+
             viewer.sync()
